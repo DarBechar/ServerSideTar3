57667f7612d88bb93c7843900292c141e975397c
Dar Bechar <Darbechar12@gmail.com>
2024-12-07 17:16:18 +0200

    Add files via upload

---
+++ AjaxCall.js
@@ -0,0 +1,14 @@
+function ajaxCall(method, api, data, successCB, errorCB) {
+  $.ajax({
+    type: method,
+    url: api,
+    data: data,
+    cache: false,
+    contentType: "application/json",
+    dataType: "json",
+    success: successCB,
+    error: errorCB,
+  });
+}
+
+
